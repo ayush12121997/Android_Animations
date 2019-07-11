279255de82f6e6d2089b428297c3b5a4ad7ed506
Ayush Shrivastava <ayush16024@iiitd.ac.in>
2019-07-11 23:27:32 +0530

    Completed animations for: 1. Rotation 2. Scaling 3. Translation 4. Fade

--- app/src/main/java/com/example/ayush/animations/MainActivity.java
+++ app/src/main/java/com/example/ayush/animations/MainActivity.java
@@ -2,8 +2,47 @@ package com.example.ayush.animations;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean check = true;
+    boolean firsttime = true;
+
+    public void fade(View view)
+    {
+        Log.i("Info", "Image Tapped");
+        ImageView imageView = (ImageView)findViewById(R.id.imageView);
+        ImageView imageView2 = (ImageView)findViewById(R.id.imageView2);
+        if(check && firsttime)
+        {
+            imageView.animate().alpha(0).setDuration(2000);
+            imageView.animate().translationYBy(1500).scaleX(0.1f).scaleY(0.1f).rotation(720).setDuration(2000);
+            imageView2.animate().alpha(1).setDuration(2000);
+            check = false;
+            firsttime = false;
+        }
+        else if(check)
+        {
+            imageView.animate().alpha(0).setDuration(2000);
+            imageView.animate().translationYBy(1500).scaleX(0.1f).scaleY(0.1f).rotation(720).setDuration(2000);
+            imageView2.animate().alpha(1).setDuration(2000);
+            imageView2.animate().translationYBy(-1500).scaleX(1).scaleY(1).setDuration(2000);
+            check = false;
+        }
+        else
+        {
+            imageView2.animate().alpha(0).setDuration(2000);
+            imageView2.animate().translationYBy(1500).scaleX(0.1f).scaleY(0.1f).rotation(720).setDuration(2000);
+            imageView.animate().alpha(1).setDuration(2000);
+            imageView.animate().translationYBy(-1500).scaleX(1).scaleY(1).setDuration(2000);
+            check = true;
+        }
+        imageView.setRotation(0);
+        imageView2.setRotation(0);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
